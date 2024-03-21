e01215a636ba20ea32cb8bad7dcda88ef042b5c1
shelbytraylor <112023993+shelbytraylor@users.noreply.github.com>
2024-03-20 19:32:20 -0600

    ICA10

--- ica/ica10.js
+++ ica/ica10.js
@@ -1,23 +1,46 @@
+
+// <script>
+//     const button1 = document.querySelector("button");
+    
+//     button.addEventListener('click', changeText);
+
+//     const heading = document.querySelector("h1")
+
+//     function changeText() {
+//         const headingText = prompt("What are you excited for?");
+//         heading.textContent = headingText;
+//     }
+
+//     const button2 = document.querySelector("button");
+    
+//     button.addEventListener('click', changeText);
+
+//     const heading = document.querySelector("h1")
+
+//     function changeText() {
+//         const headingText = prompt("What are you excited for?");
+//         heading.textContent = headingText;
+//     }
+//     </script>
+
+
 <script>
-    const button1 = document.querySelector("button");
-    
-    button.addEventListener('click', changeText);
+    const button1 = document.querySelector(".button1");
+    button1.addEventListener('click', changeText);
 
-    const heading = document.querySelector("h1")
+    const button2 = document.querySelector(".button2");
+    button2.addEventListener('click', changeColor);
 
-    function changeText() {
-        const headingText = prompt("What are you excited for?");
-        heading.textContent = headingText;
-    }
-
-    const button2 = document.querySelector("button");
-    
-    button.addEventListener('click', changeText);
-
-    const heading = document.querySelector("h1")
+    const heading = document.querySelector("h1");
 
     function changeText() {
-        const headingText = prompt("What are you excited for?");
-        heading.textContent = headingText;
+        const headingText = prompt("Type a funny phrase to see it as the header of the website");
+        heading.textContent = "Cool!";
     }
-    </script>
+
+    function changeColor() {
+        const bgColor = prompt("type the word pink to see a cool effect:");
+        document.body.style.backgroundColor = bgColor;
+    }
+
+</script>
---
+++ wa/wa10/wa10.js
@@ -0,0 +1,83 @@
+// const customName = document.getElementById(`customname`);
+// const randomize = document.querySelector(`.randomize`);
+// const story = document.querySelector(`.story`);
+
+// function randomValueFromArray(array){
+//     const random = Math.floor(Math.random()* array.length);
+//     return array[random];
+// }
+
+// const storyText = ['It was 75 farhenheit outside and sunny, so :insertx: went for a walk. When they got to :inserty:, they smiled in bliss, then :insertz:. Some strangers saw the whole thing, but she didn't mind this was the best day of her life.'];
+// const insertx = ['Shelby and her best friends', 'Shelby and her mom', 'Shelby and her puppy'];
+// const inserty = ['the beach', 'the coffeshop', 'the yoga studio'];
+// const insertz = ['passed away due to pure happiness', 'jumped up and down in excitement', 'hugged someone'];
+
+// randomize.addEventListener(`click`, result);
+
+// function result() {
+//     let newStory = storyText;
+//     const xItem = randomValueFromArray(insertx);
+//     const yItem = randomValueFromArray(inserty);
+//     const zItem = randomValueFromArray(insertz);
+
+//     newStory = newStory.replaceAll(':insertx:', xItem);
+//     newStory = newStory.replaceAll(':inserty:', yItem);
+//     newStory = newStory.replaceAll(':insertz:', zItem);
+
+//     if(customName.value !== '') {
+//         const name = customName.value;
+//         newStory = newStory.replaceAll('Bob', name);
+//     }
+
+//     if(document.getElementById("uk").checked) {
+//         const weight = `${Math.round(300*0.0714286)} stone`;
+//         const temperature = `${Math.round((94-32)*5/9)} centigrade`;
+//         newStory = newStory.replaceAll('94 farhenheit', temperature);
+//         newStory = newStory.replaceAll('300 pounds', weight);
+//     }
+    
+//     story.textContent = newStory;
+//     story.style.visibility = 'visible';
+// }
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array) {
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
+}
+
+const storyText = 'It was 75 farhenheit outside and sunny, so :insertx: went for a walk. When they got to :inserty:, they smiled in bliss, then :insertz:. Some strangers saw the whole thing, but she didn\'t mind this was the best day of her life.';
+const insertx = ['Shelby and her best friends', 'Shelby and her mom', 'Shelby and her puppy'];
+const inserty = ['the beach', 'the coffeshop', 'the yoga studio'];
+const insertz = ['passed away due to pure happiness', 'jumped up and down in excitement', 'hugged someone'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+    let newStory = storyText;
+    const xItem = randomValueFromArray(insertx);
+    const yItem = randomValueFromArray(inserty);
+    const zItem = randomValueFromArray(insertz);
+
+    newStory = newStory.replaceAll(':insertx:', xItem);
+    newStory = newStory.replaceAll(':inserty:', yItem);
+    newStory = newStory.replaceAll(':insertz:', zItem);
+
+    if (customName.value !== '') {
+        const name = customName.value;
+        newStory = newStory.replaceAll('', name);
+    }
+
+    if (document.getElementById('uk').checked) {
+        const weight = `${Math.round(300 * 0.0714286)} stone`;
+        const temperature = `${Math.round((75 - 32) * 5 / 9)} centigrade`;
+        newStory = newStory.replaceAll('75 farhenheit', temperature);
+        newStory = newStory.replaceAll('300 pounds', weight);
+    }
+    
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
+}
